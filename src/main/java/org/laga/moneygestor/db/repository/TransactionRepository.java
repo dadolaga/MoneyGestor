d67680d95fabf762dc02d4b73ce68afea7936426
Davide Lagazio <lagazio.davide.1f@gmail.com>
2023-11-23 17:46:51 +0100

    Create /transaction/get and edit rest service

--- src/main/java/org/laga/moneygestor/db/repository/TransactionRepository.java
+++ src/main/java/org/laga/moneygestor/db/repository/TransactionRepository.java
@@ -1,7 +1,18 @@
 package org.laga.moneygestor.db.repository;
 
+import jakarta.transaction.Transactional;
 import org.laga.moneygestor.db.entity.TransactionDb;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
 
 public interface TransactionRepository extends JpaRepository<TransactionDb, Integer> {
+    @Transactional
+    @Modifying
+    @Query(value = "UPDATE transaction SET Description = :description, Value = :value, Date = :date, Wallet = :wallet WHERE Id = :id AND User = :userId", nativeQuery = true)
+    int editWallet(@Param("id") Integer transactionId, @Param("description") String description, @Param("value") BigDecimal value, @Param("date") String date, @Param("wallet") Integer walletId, @Param("userId") Integer userId);
 }
--- src/main/java/org/laga/moneygestor/services/TransactionRest.java
+++ src/main/java/org/laga/moneygestor/services/TransactionRest.java
@@ -11,6 +11,7 @@ import org.laga.moneygestor.services.exceptions.MoneyGestorErrorSample;
 import org.laga.moneygestor.services.json.CreateWallet;
 import org.laga.moneygestor.services.json.Transaction;
 import org.laga.moneygestor.services.json.TransactionForm;
+import org.laga.moneygestor.services.json.Wallet;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Example;
@@ -20,6 +21,7 @@ import org.springframework.web.bind.annotation.*;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @RestController
 @RequestMapping("/transaction")
@@ -75,5 +77,45 @@ public class TransactionRest {
         transactionExample.setUserId(userGestor.getId());
 
         return TransactionGestor.convertToRest(transactionRepository.findAll(Example.of(transactionExample)));
+    }
+
+    @GetMapping("/get/{id}")
+    public Transaction getAllTransaction(@RequestHeader(HttpHeaders.AUTHORIZATION) String authorization, @PathVariable Integer id) {
+        var user = userRepository.findFromToken(authorization);
+        if(user == null)
+            throw MoneyGestorErrorSample.USER_NOT_FOUND;
+
+        UserGestor userGestor = UserGestor.Builder.createFromDB(user);
+
+        if(!userGestor.tokenIsValid())
+            throw MoneyGestorErrorSample.USER_TOKEN_NOT_VALID;
+
+        TransactionDb transactionExample = new TransactionDb();
+        transactionExample.setUserId(userGestor.getId());
+        transactionExample.setId(id);
+
+        try {
+            return TransactionGestor.convertToRest(transactionRepository.findOne(Example.of(transactionExample)).get());
+        } catch (NoSuchElementException ignored) {
+            throw MoneyGestorErrorSample.USER_NOT_HAVE_PERMISSION;
+        }
+    }
+
+    @PostMapping("/edit")
+    public void editTransaction(@RequestHeader(HttpHeaders.AUTHORIZATION) String authorization, @RequestBody Transaction transaction) {
+        if(authorization == null)
+            throw MoneyGestorErrorSample.LOGIN_REQUIRED;
+
+        try {
+            UserGestor userGestor = UserGestor.Builder.createFromDB(userRepository.findFromToken(authorization));
+            if(!userGestor.tokenIsValid())
+                throw MoneyGestorErrorSample.USER_TOKEN_NOT_VALID;
+
+            if(transactionRepository.editWallet(transaction.getId(), transaction.getDescription(), transaction.getValue(), transaction.getDate(), transaction.getWalletId(), userGestor.getId()) == 0)
+                throw MoneyGestorErrorSample.USER_NOT_HAVE_PERMISSION;
+
+        } catch (IllegalArgumentException e) {
+            throw MoneyGestorErrorSample.USER_NOT_FOUND;
+        }
     }
 }
