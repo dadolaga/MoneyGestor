319df76be9f1f8ca2eff9eddc5948bc4a6f3c7e8
Davide Lagazio <lagazio.davide.1f@gmail.com>
2023-11-28 11:56:59 +0100

    Create /transaction_type/new rest service

---
+++ src/main/java/org/laga/moneygestor/services/TransactionTypeRest.java
@@ -0,0 +1,76 @@
+package org.laga.moneygestor.services;
+
+import org.laga.moneygestor.db.entity.TransactionDb;
+import org.laga.moneygestor.db.entity.TransactionTypeDb;
+import org.laga.moneygestor.db.repository.TransactionTypeRepository;
+import org.laga.moneygestor.db.repository.UserRepository;
+import org.laga.moneygestor.logic.SortGestor;
+import org.laga.moneygestor.logic.TransactionGestor;
+import org.laga.moneygestor.logic.UserGestor;
+import org.laga.moneygestor.services.exceptions.MoneyGestorErrorSample;
+import org.laga.moneygestor.services.json.Transaction;
+import org.laga.moneygestor.services.json.TransactionForm;
+import org.laga.moneygestor.services.json.TransactionTypeForm;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+@RestController
+@RequestMapping("/transaction_type")
+public class TransactionTypeRest {
+    private final TransactionTypeRepository transactionTypeRepository;
+    private final UserRepository userRepository;
+
+    @Autowired
+    public TransactionTypeRest(TransactionTypeRepository transactionTypeRepository, UserRepository userRepository) {
+        this.transactionTypeRepository = transactionTypeRepository;
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping("/list")
+    public List<TransactionTypeDb> getListTransactionType(@RequestHeader(name = HttpHeaders.AUTHORIZATION) String authorization) {
+        var user = userRepository.findFromToken(authorization);
+        if(user == null)
+            throw MoneyGestorErrorSample.USER_NOT_FOUND;
+
+        UserGestor userGestor = UserGestor.Builder.createFromDB(user);
+
+        if(!userGestor.tokenIsValid())
+            throw MoneyGestorErrorSample.USER_TOKEN_NOT_VALID;
+
+        TransactionDb transactionExample = new TransactionDb();
+        transactionExample.setUserId(userGestor.getId());
+
+        return transactionTypeRepository.findAll();
+    }
+
+    @PostMapping("/new")
+    public void addNewTransactionType(@RequestHeader(HttpHeaders.AUTHORIZATION) String authorization, @RequestBody TransactionTypeForm transactionTypeForm) {
+        var user = userRepository.findFromToken(authorization);
+        if(user == null)
+            throw MoneyGestorErrorSample.USER_NOT_FOUND;
+
+        UserGestor userGestor = UserGestor.Builder.createFromDB(user);
+
+        if(!userGestor.tokenIsValid())
+            throw MoneyGestorErrorSample.USER_TOKEN_NOT_VALID;
+
+        TransactionTypeDb transactionTypeDb = new TransactionTypeDb();
+        transactionTypeDb.setName(transactionTypeForm.getName());
+        transactionTypeDb.setUserId(userGestor.getId());
+
+        try {
+            transactionTypeRepository.save(transactionTypeDb);
+        } catch (DataIntegrityViolationException e) {
+            if(e.getMessage().contains("unique_name_user"))
+                throw MoneyGestorErrorSample.DUPLICATE_NAME;
+        }
+    }
+}
---
+++ src/main/java/org/laga/moneygestor/services/json/TransactionTypeForm.java
@@ -0,0 +1,13 @@
+package org.laga.moneygestor.services.json;
+
+public class TransactionTypeForm {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
