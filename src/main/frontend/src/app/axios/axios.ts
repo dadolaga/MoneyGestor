db304f46cd13c3f2925da9b1e0a830dd93f3e0c9
Davide Lagazio <lagazio.davide.1f@gmail.com>
2024-04-06 10:22:42 +0200

    Configure all file with publish on server

--- src/main/frontend/src/app/axios/axios.ts
+++ src/main/frontend/src/app/axios/axios.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 
+// TODO change url with your RestAPI address
 export default axios.create({
     baseURL: "http://localhost:8093/api/"
 })
--- src/main/java/org/laga/moneygestor/services/controller/WebConfig.java
+++ src/main/java/org/laga/moneygestor/services/controller/WebConfig.java
@@ -2,16 +2,15 @@ package org.laga.moneygestor.services.controller;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
+//@EnableWebMvc // Remove this comment to develop with "npm run dev"
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST");
     }
 }
