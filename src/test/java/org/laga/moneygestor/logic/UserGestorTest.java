bd204a4129bea8001df2d13a24c7f4ef81b506f9
Davide Lagazio <lagazio.davide.1f@gmail.com>
2024-06-02 10:57:06 +0200

    Finalize user test

--- src/test/java/org/laga/moneygestor/logic/UserGestorTest.java
+++ src/test/java/org/laga/moneygestor/logic/UserGestorTest.java
@@ -5,17 +5,27 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.laga.moneygestor.TestUtilities;
 import org.laga.moneygestor.db.entity.UserDb;
-import org.laga.moneygestor.logic.exceptions.DuplicateValueException;
-import org.laga.moneygestor.logic.exceptions.UserNotHavePermissionException;
+import org.laga.moneygestor.logic.exceptions.*;
+import org.laga.moneygestor.services.models.UserRegistrationForm;
+
+import java.time.LocalDateTime;
+import java.util.LinkedList;
+import java.util.List;
 
 public class UserGestorTest extends LogicBaseTest {
     private UserDb userDb;
     private UserGestor userGestor;
 
+    private List<String> keyToDelete;
+
     @BeforeEach
     public void initializeUser() {
+        keyToDelete = new LinkedList<>();
+
         userGestor = new UserGestor(sessionFactory);
 
         userDb = new UserDb();
@@ -25,6 +35,8 @@ public class UserGestorTest extends LogicBaseTest {
         userDb.setUsername("TestSuit_" + TestUtilities.generateRandomString(6));
         userDb.setPassword("ThisIsNotEncrypt");
         userDb.setEmail(TestUtilities.generateEmail());
+
+        keyToDelete.add(userDb.getEmail());
     }
 
     @Test
@@ -58,11 +70,11 @@ public class UserGestorTest extends LogicBaseTest {
         secondUser.setPassword("ThisIsNotEncrypt");
         secondUser.setEmail(userDb.getEmail());
 
+        keyToDelete.add(secondUser.getEmail());
+
         Assertions.assertThrows(DuplicateValueException.class, () -> {
             userGestor.insert(null, secondUser);
         });
-
-        deleteUser(secondUser.getEmail());
     }
 
     @Test
@@ -77,11 +89,11 @@ public class UserGestorTest extends LogicBaseTest {
         secondUser.setPassword("ThisIsNotEncrypt");
         secondUser.setEmail(TestUtilities.generateEmail("second.test"));
 
+        keyToDelete.add(secondUser.getEmail());
+
         Assertions.assertThrows(DuplicateValueException.class, () -> {
             userGestor.insert(null, secondUser);
         });
-
-        deleteUser(secondUser.getEmail());
     }
 
     @Test
@@ -111,11 +123,11 @@ public class UserGestorTest extends LogicBaseTest {
         secondUser.setPassword("ThisIsNotEncrypt");
         secondUser.setEmail(email);
 
+        keyToDelete.add(email);
+
         Assertions.assertThrows(UserNotHavePermissionException.class, () -> {
             userGestor.deleteById(secondUser, userDb.getId());
         });
-
-        deleteUser(email);
     }
 
     @Test
@@ -150,16 +162,193 @@ public class UserGestorTest extends LogicBaseTest {
         secondUser.setPassword("ThisIsNotEncrypt");
         secondUser.setEmail(email);
 
+        keyToDelete.add(secondUser.getEmail());
+
         Assertions.assertThrows(UserNotHavePermissionException.class, () -> {
             userGestor.update(secondUser, userDb);
         });
+    }
 
-        deleteUser(email);
+    @Test
+    public void createUser_correctData_returnUserWithDataCorrect() throws UserCreationException {
+        var userForm = new UserRegistrationForm();
+
+        userForm.setFirstname("Test");
+        userForm.setLastname("Last");
+        userForm.setEmail("myemail@test.ts");
+        userForm.setUsername("usertest");
+        userForm.setPassword("This_Is_My_Password1");
+        userForm.setConfirm("This_Is_My_Password1");
+
+        var userDb = UserGestor.createUserFromRegistrationForm(userForm);
+
+        Assertions.assertAll(
+                () -> Assertions.assertEquals(userForm.getFirstname(), userDb.getFirstname()),
+                () -> Assertions.assertEquals(userForm.getLastname(), userDb.getLastname()),
+                () -> Assertions.assertEquals(userForm.getEmail(), userDb.getEmail()),
+                () -> Assertions.assertEquals(userForm.getUsername(), userDb.getUsername()),
+                () -> Assertions.assertNull(userDb.getToken()),
+                () -> Assertions.assertNull(userDb.getExpiratedToken())
+        );
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "giorgio.neriexample.com",   // Missing '@' symbol
+            "anna.rossi@@example.com",   // Double '@' symbol
+            "chiara. neri@example.com",  // Space in the username
+            "andrea.verdi@exam ple.com", // Space in the domain
+            ".francesca.rossi@example.com", // Leading dot in the username
+            "paolo.verdi.@example.com",  // Trailing dot in the username
+            "mario.bianchi!@example.com" // Invalid special character in the username
+    })
+    public void createUser_emailNotCorrect_throw(String email) {
+        var userForm = new UserRegistrationForm();
+
+        userForm.setFirstname("Test");
+        userForm.setLastname("Last");
+        userForm.setEmail(email);
+        userForm.setUsername("usertest");
+        userForm.setPassword("This_Is_My_Password1");
+        userForm.setConfirm("This_Is_My_Password1");
+
+        Assertions.assertThrows(UserCreationException.class, () -> UserGestor.createUserFromRegistrationForm(userForm));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "short1!",          // Less than 8 characters
+            "alllowercase1!",   // No uppercase letter
+            "ALLUPPERCASE1!",   // No lowercase letter
+            "NoNumber!",        // No number
+            "NoSpecialChar1",   // No special character
+            "Noupperlower1!",   // No uppercase letter, only lowercase
+            "12345678!",        // No uppercase or lowercase letter
+            "NoSpecialChar1",   // No special character
+            "Short1",           // Less than 8 characters
+            "NOLOWERCASE123!"   // No lowercase letter
+    })
+    public void createUser_passwordNotCorrect_throw(String password) {
+        var userForm = new UserRegistrationForm();
+
+        userForm.setFirstname("Test");
+        userForm.setLastname("Last");
+        userForm.setEmail("test@test.ts");
+        userForm.setUsername("usertest");
+        userForm.setPassword(password);
+        userForm.setConfirm(password);
+
+        Assertions.assertThrows(UserCreationException.class, () -> UserGestor.createUserFromRegistrationForm(userForm));
+    }
+
+    @Test
+    public void createUser_passwordNotEqual_throw() {
+        var userForm = new UserRegistrationForm();
+
+        userForm.setFirstname("Test");
+        userForm.setLastname("Last");
+        userForm.setEmail("test@test.ts");
+        userForm.setUsername("usertest");
+        userForm.setPassword("This_Is_My_Password1");
+        userForm.setConfirm("This_Is_My_Password2");
+
+        Assertions.assertThrows(UserPasswordNotEqualsException.class, () -> UserGestor.createUserFromRegistrationForm(userForm));
+    }
+
+    @Test
+    public void login_correctPasswordSearchByUsername_refreshToken() throws UserCreationException {
+        final String username = "userForTest_" + TestUtilities.generateRandomString(6);
+        final String email = TestUtilities.generateEmail();
+        final String password = "This_Is_My_Password1";
+        var userForm = new UserRegistrationForm();
+
+        userForm.setFirstname("Test");
+        userForm.setLastname("Test");
+        userForm.setEmail(email);
+        userForm.setUsername(username);
+        userForm.setPassword(password);
+        userForm.setConfirm(password);
+
+        keyToDelete.add(email);
+
+        var user = UserGestor.createUserFromRegistrationForm(userForm);
+
+        userGestor.insert(null, user);
+
+        var userLogged = userGestor.login(username, password);
+
+        Assertions.assertAll(
+                () -> Assertions.assertEquals(username, userLogged.getUsername()),
+                () -> Assertions.assertNotNull(userLogged.getToken()),
+                () -> Assertions.assertTrue(userLogged.getExpiratedToken().isAfter(LocalDateTime.now()))
+        );
+    }
+
+    @Test
+    public void login_correctPasswordSearchByEmail_refreshToken() throws UserCreationException {
+        final String username = "userForTest_" + TestUtilities.generateRandomString(6);
+        final String email = TestUtilities.generateEmail();
+        final String password = "This_Is_My_Password1";
+        var userForm = new UserRegistrationForm();
+
+        userForm.setFirstname("Test");
+        userForm.setLastname("Test");
+        userForm.setEmail(email);
+        userForm.setUsername(username);
+        userForm.setPassword(password);
+        userForm.setConfirm(password);
+
+        keyToDelete.add(email);
+
+        var user = UserGestor.createUserFromRegistrationForm(userForm);
+
+        userGestor.insert(null, user);
+
+        var userLogged = userGestor.login(email, password);
+
+        Assertions.assertAll(
+                () -> Assertions.assertEquals(email, userLogged.getEmail()),
+                () -> Assertions.assertNotNull(userLogged.getToken()),
+                () -> Assertions.assertTrue(userLogged.getExpiratedToken().isAfter(LocalDateTime.now()))
+        );
+    }
+
+    @Test
+    public void login_passwordNotEqual_throw() throws UserCreationException {
+        final String username = "userForTest_" + TestUtilities.generateRandomString(6);
+        final String email = TestUtilities.generateEmail();
+        final String password = "This_Is_My_Password1";
+        var userForm = new UserRegistrationForm();
+
+        userForm.setFirstname("Test");
+        userForm.setLastname("Test");
+        userForm.setEmail(email);
+        userForm.setUsername(username);
+        userForm.setPassword(password);
+        userForm.setConfirm(password);
+
+        keyToDelete.add(email);
+
+        var user = UserGestor.createUserFromRegistrationForm(userForm);
+
+        userGestor.insert(null, user);
+
+        Assertions.assertThrows(UserPasswordNotEqualsException.class, () -> userGestor.login(email, password));
+    }
+
+    @Test
+    public void login_emailOrUsernameNotFound_throw() throws UserCreationException {
+        String usernameOrEmail = TestUtilities.generateEmail("ThisEmailNotExist");
+        var userGestor = new UserGestor(sessionFactory);
+
+        Assertions.assertThrows(UserNotFoundException.class, () -> userGestor.login(usernameOrEmail, "Password1!"));
     }
 
     @AfterEach
     public void deleteUserInsert() {
-        deleteUser(userDb.getEmail());
+        for (var toDelete : keyToDelete) {
+            deleteUser(toDelete);
+        }
     }
 
     public void deleteUser(String email) {
