1f77f6db9d8f556714f809b568b31b35b2872da4
Davide Lagazio <lagazio.davide.1f@gmail.com>
2024-06-02 18:55:29 +0200

    Fixed transaction type gestor

--- src/main/java/org/laga/moneygestor/logic/TransactionTypeGestor.java
+++ src/main/java/org/laga/moneygestor/logic/TransactionTypeGestor.java
@@ -1,6 +1,5 @@
 package org.laga.moneygestor.logic;
 
-import jakarta.persistence.EntityNotFoundException;
 import org.hibernate.Session;
 import org.hibernate.SessionException;
 import org.hibernate.SessionFactory;
@@ -8,13 +7,11 @@ import org.hibernate.Transaction;
 import org.hibernate.exception.ConstraintViolationException;
 import org.laga.moneygestor.db.entity.TransactionTypeDb;
 import org.laga.moneygestor.db.entity.UserDb;
-import org.laga.moneygestor.exceptions.NotImplementedMethod;
 import org.laga.moneygestor.logic.exceptions.DuplicateValueException;
 import org.laga.moneygestor.logic.exceptions.TableNotEmptyException;
 import org.laga.moneygestor.logic.exceptions.UserNotHavePermissionException;
 
 import java.util.List;
-import java.util.Objects;
 
 public class TransactionTypeGestor extends Gestor<Integer, TransactionTypeDb> {
 
@@ -56,13 +53,10 @@ public class TransactionTypeGestor extends Gestor<Integer, TransactionTypeDb> {
         TransactionTypeDb transactionTypeDb = getById(session, userLogged, id);
 
         if(transactionTypeDb == null)
-            throw new EntityNotFoundException("Wallet with " + id + " not found");
+            throw new UserNotHavePermissionException();
 
         if(!forceDelete && !transactionTypeDb.getTransaction().isEmpty())
             throw new TableNotEmptyException("transaction table is not empty");
-
-        if(!Objects.equals(userLogged.getId(), transactionTypeDb.getUserId()))
-            throw new UserNotHavePermissionException();
 
         session.remove(session.contains(transactionTypeDb) ? transactionTypeDb : session.merge(transactionTypeDb));
 
@@ -70,18 +64,43 @@ public class TransactionTypeGestor extends Gestor<Integer, TransactionTypeDb> {
     }
 
     @Override
-    public void update(UserDb userLogged, TransactionTypeDb newObject) {
-        throw new NotImplementedMethod();
+    public void update(UserDb userLogged, TransactionTypeDb newTransactionTypeDb) {
+        update(userLogged, newTransactionTypeDb.getId(), newTransactionTypeDb);
     }
 
     @Override
-    public void update(Session session, UserDb userLogged, Integer integer, TransactionTypeDb newObject) {
-        throw new NotImplementedMethod();
+    public void update(Session session, UserDb userLogged, Integer id, TransactionTypeDb newTransactionTypeDb) {
+        if(userLogged == null || newTransactionTypeDb == null || id == null)
+            throw new IllegalArgumentException("one or more argument is null");
+
+        if(sessionFactory == null)
+            throw new SessionException("Session is null");
+
+        try {
+            Transaction transaction = session.getTransaction();
+
+            var transactionType = getById(session, userLogged, id);
+
+            if(transactionType == null)
+                throw new UserNotHavePermissionException();
+
+            transactionType.setName(newTransactionTypeDb.getName());
+
+            session.persist(transactionType);
+
+            transaction.commit();
+        } catch (ConstraintViolationException e) {
+            if(e.getMessage().contains("index_transactionType_nameuser"))
+                throw new DuplicateValueException("Duplicate value for wallet", e);
+        }
     }
 
     @Override
     public TransactionTypeDb getById(Session session, UserDb userLogged, Integer id) {
-        return session.get(TransactionTypeDb.class, id);
+        return session.createQuery("FROM TransactionTypeDb WHERE id = :id AND userId = :userId", TransactionTypeDb.class)
+                .setParameter("id", id)
+                .setParameter("userId", userLogged.getId())
+                .getSingleResultOrNull();
     }
 
     @Override
