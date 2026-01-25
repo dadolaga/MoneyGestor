7053c8e801e241fd6fe49a7940f01598b036f4ba
Davide Lagazio <lagazio.davide.1f@gmail.com>
2026-01-25 16:53:09 +0100

    Update editorconfig

---
+++ backend/logic/Commands/FindUserByTokenCommand.cs
@@ -0,0 +1,18 @@
+﻿using logic.Models;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace logic.Commands
+{
+    public class FindUserByTokenCommand : ICommand<UInt64>
+    {
+        internal override Task Execute(ExecutorManager executorManager)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
