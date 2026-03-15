8b33ed14e53a8778b3ae3f82b80b7ef6be9f7962
Davide Lagazio <lagazio.davide.1f@gmail.com>
2026-03-15 19:01:20 +0100

    Add favorite command

---
+++ backend/TestProject/Commands/Wallets/FavoriteWalletTest .cs
@@ -0,0 +1,76 @@
+﻿using logic;
+using logic.Commands.Wallets;
+using logic.Exceptions;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestProject.Base;
+using TestProject.Samples;
+using TestProject.Utils;
+
+namespace TestProject.Commands.Wallets {
+    internal class FavoriteWalletTest : ExecutorBase {
+        private UserSample user;
+        private LoginSample login;
+        private WalletSample wallet;
+
+        [SetUp]
+        public async Task Setup() {
+            user = new UserSample();
+            login = new LoginSample(user);
+            wallet = new WalletSample();
+
+            await ExecutorManager.Execute(user.AddNewUserExecutor);
+            await ExecutorManager.Execute(login.LoginCommand);
+
+            ExecutorManager.SetToken(login.LoginCommand.Result);
+
+            await ExecutorManager.Execute(wallet.AddWalletCommand);
+        }
+
+        [TearDown]
+        public async Task Teardown() {
+            using var db = DatabaseFactory.Use();
+
+            await db.Wallets.ExecuteDeleteAsync();
+            await db.Logins.ExecuteDeleteAsync();
+            await db.Users.ExecuteDeleteAsync();
+        }
+
+        [Test]
+        public async Task EffectiveSetFavorite() {
+            var favoriteWalletCommand = new FavoriteWalletCommand(walletId: wallet.AddWalletCommand.Result, favorite: true);
+
+            await ExecutorManager.Execute(favoriteWalletCommand);
+
+            using var db = DatabaseFactory.Use();
+            var walletUpdated = await db.Wallets.FirstAsync(w => w.Id == wallet.AddWalletCommand.Result);
+
+            Assert.That(walletUpdated.Name, Is.EqualTo(wallet.Name));
+            Assert.That(walletUpdated.ColorId, Is.EqualTo(wallet.ColorId));
+            Assert.That(walletUpdated.Value, Is.EqualTo(wallet.Value));
+            Assert.That(walletUpdated.Favorite, Is.True);
+        }
+
+        [Test]
+        public async Task DuplicateWalletNameDifferentUser() {
+            var secondUser = new UserSample(username: $"{user.Username}_1", email: $"{user.Email}_1");
+            var secondExecutor = await TestUtils.CreateExecutorManagerForUser(secondUser);
+            
+            var favoriteWalletCommand = new FavoriteWalletCommand(walletId: wallet.AddWalletCommand.Result, favorite: true);
+
+            Assert.ThrowsAsync<ObjectNotFoundException>(async () => await secondExecutor.Execute(favoriteWalletCommand));
+
+            using var db = DatabaseFactory.Use();
+            var walletUpdated = await db.Wallets.FirstAsync(w => w.Id == wallet.AddWalletCommand.Result);
+
+            Assert.That(walletUpdated.Name, Is.EqualTo(wallet.Name));
+            Assert.That(walletUpdated.ColorId, Is.EqualTo(wallet.ColorId));
+            Assert.That(walletUpdated.Value, Is.EqualTo(wallet.Value));
+            Assert.That(walletUpdated.Favorite, Is.False);
+        }
+    }
+}
---
+++ backend/logic/Commands/Wallets/FavoriteWalletCommand.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using logic.Exceptions;
+using Microsoft.EntityFrameworkCore;
+
+namespace logic.Commands.Wallets {
+    public class FavoriteWalletCommand : ICommand<UInt64> {
+        private UInt64 walletId;
+        private Boolean favorite;
+
+        public FavoriteWalletCommand(UInt64 walletId, Boolean favorite) {
+            this.walletId = walletId;
+            this.favorite = favorite;
+        }
+
+        internal override async Task Execute(ExecutorManager executorManager) {
+            var database = executorManager.DbContext;
+
+            var wallet = database.Wallets.FirstOrDefault(w => w.Id == walletId && w.UserId == executorManager.UserId) ?? throw new ObjectNotFoundException("Wallet not found");
+
+            wallet.Favorite = favorite;
+
+            database.Update(wallet);
+
+            await database.SaveChangesAsync();
+
+            result = wallet.Id;
+        }
+    }
+}
