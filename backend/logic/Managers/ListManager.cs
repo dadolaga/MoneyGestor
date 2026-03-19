723d28669f7dbd5c12126ac4c725d8cafb85b2f2
Davide Lagazio <lagazio.davide.1f@gmail.com>
2026-03-19 21:06:46 +0100

    Fix list order filter

--- backend/logic/Managers/ListManager.cs
+++ backend/logic/Managers/ListManager.cs
@@ -11,20 +11,32 @@ namespace logic.Managers {
     public static class ListManager {
         public static IOrderedQueryable<T> OrderByPropertyName<T>(this IQueryable<T> source, String propertyName, Boolean descending, Boolean first = true) {
             var type = typeof(T);
-            var property = type.GetProperty(propertyName, BindingFlags.IgnoreCase | BindingFlags.Public | BindingFlags.Instance);
+            var parameter = Expression.Parameter(type, "p");
+            Expression propertyAccess = parameter;
 
-            if (property == null) {
-                return (IOrderedQueryable<T>) source;
+            foreach (var part in propertyName.Split('.')) {
+                PropertyInfo? property = propertyAccess.Type.GetProperty(part, BindingFlags.IgnoreCase | BindingFlags.Public | BindingFlags.Instance);
+
+                if (property == null) {
+                    return (IOrderedQueryable<T>) source; 
+                }
+
+                propertyAccess = Expression.MakeMemberAccess(propertyAccess, property);
             }
 
-            var parameter = Expression.Parameter(type, "p");
-            var propertyAccess = Expression.MakeMemberAccess(parameter, property);
             var orderByExp = Expression.Lambda(propertyAccess, parameter);
 
-            String methodName = first ? (descending ? "OrderByDescending" : "OrderBy") : (descending ? "ThenByDescending" : "ThenBy");
-            var resultExp = Expression.Call(typeof(Queryable), methodName,
-                                new System.Type[] { type, property.PropertyType },
-                                source.Expression, Expression.Quote(orderByExp));
+            String methodName = first
+                ? (descending ? "OrderByDescending" : "OrderBy")
+                : (descending ? "ThenByDescending" : "ThenBy");
+
+            var resultExp = Expression.Call(
+                typeof(Queryable),
+                methodName,
+                new System.Type[] { type, propertyAccess.Type },
+                source.Expression,
+                Expression.Quote(orderByExp)
+            );
 
             return (IOrderedQueryable<T>) source.Provider.CreateQuery<T>(resultExp);
         }
@@ -42,7 +54,6 @@ namespace logic.Managers {
 
             return newSource;
         }
-
 
         public static IOrderedQueryable<T> ApplyFilter<T>(this IQueryable<T> source, ListFilter filter) {
             source = source.Skip((Int32) (filter.Offset * filter.Limit)).Take((Int32) filter.Limit);
